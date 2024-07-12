b4e8c2385398ed95b72b5852f19ca3feadd173cd
Hazellie <ishazelpie@gmail.com>
2024-07-12 15:07:46 +0300

    Removed HTTP plugin keybinds.

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -6,7 +6,5 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-map("n", "<leader>rr","<cmd>Rest run<cr>",  { desc = "Run request under the cursor"})
-map("n", "<leader>rl", "<cmd>Rest run last<cr>", { desc ="Re-run latest request" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
