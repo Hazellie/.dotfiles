9329abba304b123da431c1931e220360d8daf953
Hazellie <ishazelpie@gmail.com>
2024-12-02 10:20:03 +0200

    Some more random fixes, but indent is still broken, thanks neovim.

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -3,7 +3,7 @@ require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
 
-local servers = { "lua_ls","html", "cssls", "gopls", "ts_ls", "jsonls", "sqlls", "taplo" } --, "csharp_ls" }
+local servers = { "lua_ls", "html", "cssls", "gopls", "ts_ls", "jsonls", "sqlls", "taplo" } --, "csharp_ls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
@@ -14,3 +14,30 @@ for _, lsp in ipairs(servers) do
     capabilities = nvlsp.capabilities,
   }
 end
+
+lspconfig.lua_ls.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using
+        -- (most likely LuaJIT in the case of Neovim)
+        version = "LuaJIT",
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {
+          "vim",
+          "require",
+        },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
